ab85f06604dd995d437b133a64ff2e36eb00b04c
Emma Grey <emgrey02@gmail.com>
2021-05-12 23:56:02 -0400

    Updates projects section and background

---
+++ script.js
@@ -0,0 +1,12 @@
+let projects = document.querySelector('#projects');
+
+let google = projects.querySelector('#google');
+let rps = projects.querySelector('#rps');
+let etch = projects.querySelector('#etch');
+let calc = projects.querySelector('#calc');
+
+google.addEventListener('mouseover', addScreen);
+
+const addScreen = () => {
+  
+}
