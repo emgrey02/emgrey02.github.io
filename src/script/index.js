66e3711005f1a696f1a5d12bf525f008b526f330
Emma Grey <emgrey02@gmail.com>
2022-04-09 21:44:20 -0400

    Fix instersection observer animation

--- src/script/index.js
+++ src/script/index.js
@@ -1,7 +1,28 @@
 const lines = document.querySelectorAll('.moving-lines__line');
-const projects = document.querySelector('.projects');
-console.log(projects);
+
+const first = document.querySelector('.first');
+const second = document.querySelector('.second');
+
 const className = 'in-view';
+
+first.classList.remove(className);
+second.classList.remove(className);
+
+const observer = new IntersectionObserver(entries => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add(className) 
+            return;
+        }
+        entry.target.classList.remove(className);
+    });
+},
+{
+    threshold: 0
+});
+
+observer.observe(first);
+observer.observe(second);
 
 window.addEventListener('scroll', () => {
     lines.forEach((line) => {
@@ -14,20 +35,3 @@ lines.forEach((line) => {
         line.classList.remove('moving');
     })
 })
-
-projects.classList.remove(className);
-
-const observer = new IntersectionObserver(entries => {
-    entries.forEach((entry) => {
-        if (entry.isIntersecting) {
-            entry.target.classList.add(className) 
-            return;
-        }
-        entry.target.classList.remove(className);
-    });
-},
-{
-    threshold: .3
-});
-
-observer.observe(projects);
