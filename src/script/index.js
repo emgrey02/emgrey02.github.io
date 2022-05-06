4585474f464a847b9fa02575d167b4f8a1619ce1
Emma Grey <emgrey02@gmail.com>
2022-05-06 16:29:52 -0400

    Format with prettier

--- src/script/index.js
+++ src/script/index.js
@@ -8,30 +8,32 @@ const className = 'in-view';
 first.classList.remove(className);
 second.classList.remove(className);
 
-const observer = new IntersectionObserver(entries => {
-    entries.forEach((entry) => {
-        if (entry.isIntersecting) {
-            entry.target.classList.add(className) 
-            return;
-        }
-        entry.target.classList.remove(className);
-    });
-},
-{
-    threshold: 0
-});
+const observer = new IntersectionObserver(
+	(entries) => {
+		entries.forEach((entry) => {
+			if (entry.isIntersecting) {
+				entry.target.classList.add(className);
+				return;
+			}
+			entry.target.classList.remove(className);
+		});
+	},
+	{
+		threshold: 0,
+	}
+);
 
 observer.observe(first);
 observer.observe(second);
 
 window.addEventListener('scroll', () => {
-    lines.forEach((line) => {
-        line.classList.add('moving');
-    });
+	lines.forEach((line) => {
+		line.classList.add('moving');
+	});
 });
 
 lines.forEach((line) => {
-    line.addEventListener('animationend', () => {
-        line.classList.remove('moving');
-    })
-})
+	line.addEventListener('animationend', () => {
+		line.classList.remove('moving');
+	});
+});
