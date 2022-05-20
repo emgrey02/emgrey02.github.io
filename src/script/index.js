ee0c57a886d11d116e7e5f1f0721fcd6eacb6e34
Emma Grey <emgrey02@gmail.com>
2022-05-19 23:15:07 -0400

    Play around with svg

--- src/script/index.js
+++ src/script/index.js
@@ -2,11 +2,52 @@ const lines = document.querySelectorAll('.moving-lines__line');
 const body = document.querySelector('body');
 const radio = document.querySelectorAll('input[name="theme"]');
 const currentSetting = document.querySelector('.current-setting__name');
+const emma = document.querySelector('#emma-cartoon');
+
+const rightPupil = document.querySelector('#right-pupil');
+const leftPupil = document.querySelector('#left-pupil');
+
+const maxTrans = 10;
+const maxXDist = innerWidth / 2;
+const maxYDist = innerHeight / 2;
+
+const leftPupilArea = leftPupil.getBoundingClientRect();
+const rightPupilArea = rightPupil.getBoundingClientRect();
+
+const radius = leftPupilArea.width / 2;
+
+const leftCenterX = leftPupilArea.left + radius;
+const rightCenterX = rightPupilArea.left + radius;
+const leftCenterY = leftPupilArea.top + radius;
+const rightCenterY = rightPupilArea.top + radius;
 
 const first = document.querySelector('.first');
 const second = document.querySelector('.second');
 const third = document.querySelector('.third');
 const fourth = document.querySelector('.fourth');
+
+document.addEventListener('mousemove', (e) => {
+	let leftx = e.clientX - leftCenterX;
+	let rightx = e.clientX - rightCenterX;
+	let lefty = e.clientY - leftCenterY;
+	let righty = e.clientY - rightCenterY;
+
+	let leftTheta = Math.atan2(lefty, leftx);
+	let rightTheta = Math.atan2(righty, rightx);
+
+	let leftAngle = (leftTheta * 180) / Math.PI + 180;
+	let rightAngle = (rightTheta * 180) / Math.PI + 180;
+
+	leftPupil.style.transformOrigin = `center center`;
+	leftPupil.style.transform = `translateX(${radius + 'px'}) rotate(${
+		leftAngle + 'deg'
+	})`;
+
+	rightPupil.style.transform = `translateX(${radius + 'px'}) rotate(${
+		rightAngle + 'deg'
+	})`;
+	rightPupil.style.transformOrigin = `center center`;
+});
 
 const className = 'in-view';
 
